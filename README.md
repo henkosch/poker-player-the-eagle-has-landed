6a66285c9d2a2bd8447b192275bb251421ab9120
Benjamin Christian Nelson <bcnelson@instructure.com>
2019-04-13 10:47:40 -0600

    add basic bet scaffold

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 TypeScript client skeleton for Lean Poker For more information visit: http://leanpoker.org
 
-# How to get started 
+# How to get started
 
 ```bash
 $ npm install
--- src/Player.ts
+++ src/Player.ts
@@ -2,7 +2,21 @@ import { GameState } from "./GameState";
 
 export class Player {
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
-    betCallback(gameState.minimum_raise);
+    if(this.should_raise(gameState)) {
+      betCallback(this.raise_amount(gameState))
+    } else {
+      //check
+      betCallback(0);
+    }
+  }
+
+  should_raise(gameState: GameState) : boolean {
+    return true
+  }
+
+   raise_amount(gameState: GameState) : number {
+    // TODO ret amount
+    return gameState.minimum_raise
   }
 
   public showdown(gameState: GameState): void {
