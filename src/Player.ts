e835ea14dad71d3136dafafc06b9ff15f5a7fcee
Benjamin Christian Nelson <bcnelson@instructure.com>
2019-04-13 14:31:13 -0600

    add ranking from rainman

--- src/Player.ts
+++ src/Player.ts
@@ -1,22 +1,36 @@
-import { GameState } from "./GameState";
+import { GameState, Card } from "./GameState";
+import {Score} from "./Score";
+import * as _ from 'lodash';
+
+export interface Player {
+  hole_cards: Card[]
+  gamestate: GameState
+}
 
 export class Player {
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
-    if(this.should_raise(gameState)) {
-      betCallback(this.raise_amount(gameState))
-    } else {
-      //check
-      betCallback(0);
+    this.gamestate = gameState
+    if(this.hole_cards.length == 0) {
+      gameState.players.forEach((player, index) => {
+        if(player.hole_cards != null) {
+          player.hole_cards.forEach( (card, _) => {
+            this.hole_cards.push(card);
+          })
+        }
+      })
     }
+
+    this.bet(betCallback)
   }
 
-  should_raise(gameState: GameState) : boolean {
-    return true
-  }
-
-   raise_amount(gameState: GameState) : number {
-    // TODO ret amount
-    return gameState.minimum_raise
+  bet(callback) {
+    if (this.gamestate.community_cards.length > 0) {
+        var cards = _.merge(this.hole_cards, this.gamestate.community_cards)
+        var s = new Score()
+        s.scoreHand(cards, this.gamestate, callback)
+    } else {
+      callback(0)
+    }
   }
 
   public showdown(gameState: GameState): void {
--- src/Score.ts
+++ src/Score.ts
@@ -1,4 +1,7 @@
-import { Card } from "./GameState";
+import { RainmanResult } from './Score';
+import * as _ from 'lodash';
+import { GameState, Card } from "./GameState";
+import { get } from "request";
 
 export interface RainmanResult {
   rank: number;
@@ -12,7 +15,18 @@ export interface RainmanResult {
 export class Score {
   rankUrl = "http://rainman.leanpoker.org/rank"
 
-
-  public async scoreHand(cards: Card[], callback: (result: RainmanResult) => any) {
+  // betCallback: (bet: number) => void
+  public scoreHand(cards: Card[], state: GameState, callback: (int) => void) {
+    this.rankUrl + "?cards=" + JSON.stringify(cards)
+    get(this.rankUrl, {}, (result) => {
+      var rainman = <RainmanResult>JSON.parse(result.body.toString())
+      if(rainman.rank > 0){
+        callback(state.minimum_raise)
+      } else {
+        callback(0)
+      }
+    })
   }
+
+
 }
