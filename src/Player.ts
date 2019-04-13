91e2cad964a6a5bb023d542db2e5697f668854e2
Henrik Schnell <hschnell@instructure.com>
2019-04-13 10:40:58 -0600

    GameState typings.

--- src/Player.ts
+++ src/Player.ts
@@ -1,9 +1,11 @@
+import { GameState } from "./GameState";
+
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
+  public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     betCallback(100);
   }
 
-  public showdown(gameState: any): void {
+  public showdown(gameState: GameState): void {
 
   }
 };
