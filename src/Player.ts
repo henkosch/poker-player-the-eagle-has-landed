985571e1fbf7e8437037d1d8310e364e8f21fef8
Henrik Schnell <hschnell@instructure.com>
2019-04-13 10:42:33 -0600

    Change bet to minimumRaise.

--- src/Player.ts
+++ src/Player.ts
@@ -2,7 +2,7 @@ import { GameState } from "./GameState";
 
 export class Player {
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
-    betCallback(100);
+    betCallback(gameState.minimum_raise);
   }
 
   public showdown(gameState: GameState): void {
